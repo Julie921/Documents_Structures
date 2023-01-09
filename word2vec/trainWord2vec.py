e87240b0998c6610835bd53c43e869ef7bd4df15
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-01-09 10:59:54 +0100

    suppresion de la ligne pour load un modèle pré-entrainé -> on va le faire avec glove parce que glove a un modèle spécifique pour les tweets

--- word2vec/trainWord2vec.py
+++ word2vec/trainWord2vec.py
@@ -24,8 +24,6 @@ if __name__=="__main__":
         
     corpus_clean = [preprocess(sentence, pattern_clean) for sentence in text]
     
-    model = gensim.models.Word2Vec.load("./GoogleNews-vectors-negative300.bin")
-
     model = gensim.models.Word2Vec(min_count=1, vector_size=250, window=10, workers=4)
     model.build_vocab(corpus_clean)  # prepare the model vocabulary
     model.train(corpus_clean, total_examples=model.corpus_count, epochs=model.epochs)  # train word vectors
