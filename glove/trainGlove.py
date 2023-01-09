3f328678dc65a03aa412d533115e6d84edbdc5d9
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-01-09 10:59:24 +0100

    suppression d'un commentaire -> maintenant il est dans le README_glove.md

--- glove/trainGlove.py
+++ glove/trainGlove.py
@@ -1,11 +1,3 @@
-# One advantage of GloVe is that it can capture both the local context and the global statistical information of a word, 
-# whereas word2vec only captures local context. 
-# This makes GloVe more effective at capturing the meaning of rare and ambiguous words, 
-# and it can also handle polysemous words more effectively. 
-# However, word2vec is generally faster to train and requires less memory, 
-# so it may be more practical for large-scale applications.
-
-
 import gensim.downloader as api
 
 # info = api.info()  # return dict with info about available models/datasets
