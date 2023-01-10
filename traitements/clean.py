eb2fb0bd3c38460bc4666b84b76a7233d3f8ee66
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-01-10 18:43:10 +0100

    ajout d'une fonction dans clean.py pour clean tout un conll, et pas juste un token donné en entrée. + - modification de la fonction convertConll pour bien segmenter la partie nettoyage de la partie conversion : avant le nettoyage se faisait pendant la conversion

--- traitements/clean.py
+++ traitements/clean.py
@@ -1,4 +1,11 @@
 import re
+from typing import List
+from misc import safe_list_get
+import sys
+
+
+####################################################################################
+
 
 def clean_token(token: str) -> str:
     """Nettoyage des tokens de tweet.
@@ -10,22 +17,25 @@ def clean_token(token: str) -> str:
     - liens -> "@link"
     - numéros -> "!digits
     
+    Tout est mis en minuscules.
+    
     Args:
         token (str): Le token à nettoyer.
     
     Returns:
-        str: Le token nettoyé.
+        str: Le token nettoyé et mis en minuscules
     """
     if re.match("@\w", token): # anonymisation des utilisateurs
         return "@user"
-    elif re.match("COL", token):
-        return ":"
-    elif token.startswith("httpCOL//"): # on enlève les liens
+    elif token.startswith("httpCOL//") or token.startswith("httpsCOL//"): # on enlève les liens
         return "@link"
+    elif re.search("COL", token):
+        return re.sub("COL", ":", token).lower()
     elif re.match("\d", token): # on remplace les chiffres
         return "!digits"
     else: # on renvoie le token non modifié 
-        return token
+        return token.lower()
+    
 
 def clean_texte_from_text_grid(texte: str) -> str:
     """
@@ -47,3 +57,60 @@ def clean_texte_from_text_grid(texte: str) -> str:
     clean_texte = re.sub(r"\<.+\>", "", clean_texte).replace("\n \n", "\n") # on enlève les <laugh> et les lignes vides qui ont pu apparaître à cause de ça
     clean_texte = re.sub(r"\(.+\)", "", clean_texte).replace("\n \n", "\n") # on enlève (laughing)
     return clean_texte.lower() # on met tout en minuscules
+
+def clean_conll(conll_file: str) -> List[str]:
+    """Nettoie un fichier CoNLL donné en entrée.
+    
+    En plus d'écriture le CoNLL nettoyé dans un nouveau fichier, la fonction renvoie le fichier nettoyé 
+    sous la forme d'une liste de chaînes de caractères pour pouvoir l'exploiter si besoin.    
+
+    Args:
+        conll_file (str): Le chemin d'accès au fichier CoNLL qui doit être nettoyé.
+
+    Returns:
+        clean_conll_as_list (List[str]): Une liste de chaînes de caractères, où chaque chaîne est une ligne du fichier CoNLL nettoyé.
+    """
+
+    clean_file_as_list = [] # le fichier propre sous la forme d'une liste de strings
+
+    with open(f"clean_{conll_file.split('/')[-1]}", "w", encoding="utf-8") as output:
+        
+        with open(conll_file, "r", encoding="utf-8") as input:
+            l = input.readline()
+            
+            while l:
+                
+                if safe_list_get(l.split("\t"), 1, False): # si on se trouve bien sur une ligne avec les colonnes du conll
+                    l_list = l.split("\t")
+                    l_list[1] = clean_token(l_list[1]) # nettoyage du token
+                    l_list[4] = "_" # on enlève ce qui se trouve dans la cinquime colonne
+                    l_clean = "\t".join(l_list)
+                    output.write(l_clean) # écriture de la ligne propre
+                    clean_file_as_list.append(l_clean) # ajout de la ligne dans notre liste de lignes au cas où
+                
+                else: # il ne s'agit pas d'une ligne avec un token
+                    clean_file_as_list.append(l)
+                    output.write(l)
+                    
+                l = input.readline()
+                    
+    return clean_file_as_list
+
+def main():
+    """
+    Exemple de comment lancer le script : 
+    
+    ```bash
+    python3 clean.py file.conllu
+    ```
+    """
+    
+    conll_file = sys.argv[1]
+    
+    clean_conll(conll_file)
+
+####################################################################################
+
+if __name__=="__main__":
+    
+    main()
--- traitements/cluster_converter.py
+++ traitements/cluster_converter.py
@@ -1,28 +1,8 @@
 import csv
 import re
 import sys
-from typing import List, Any
-from clean import clean_token
-
-def safe_list_get(l:List[Any], idx:int, default:Any)->Any:
-    """
-    Cette fonction prend en entrée une liste, un indice et une valeur par défaut. Elle renvoie l'élément de la liste à l'indice spécifié, si l'indice est en dehors des limites de la liste, elle retourne la valeur par défaut.
-    Cela permet d'éviter les erreurs d'indexation qui pourraient se produire lors de l'accès à un élément de la liste.
-    
-    Args:
-    - l (List[Any]): une liste
-    - idx (int): un indice
-    - default: valeur par défaut 
-    
-    Returns:
-    - Any : l'élément de la liste à l'indice spécifié ou la valeur par défaut.
-    """
-    try:
-        return l[idx]
-    except IndexError:
-        return default
-
-
+from typing import List
+from misc import safe_list_get
 
 def makeDictFromTSV(tsv_file: str) -> dict:
     """
@@ -61,7 +41,7 @@ def makeDictFromTSV(tsv_file: str) -> dict:
 
 
 def convertConll(conll_file:str, converter:dict)->List[str]:
-    """Cette fonction prend en entrée un fichier CoNLL 
+    """Cette fonction prend en entrée un fichier CoNLL déjà nettoyé
     et un dictionnaire de correspondances de remplacement (forme -> nom_cluster). 
     Elle sert à exploiter les clusters créés avec l'algorithme de Brown.
     
@@ -70,7 +50,7 @@ def convertConll(conll_file:str, converter:dict)->List[str]:
     Elle conserve la forme originale du token dans la colonne `misc` du CoNLL.
     
     Le nom du nouveau CoNLL porte la mention `clustered_`. En plus de l'écriture
-    du fichier, le contenu du fichier est retourné sous forme d'une liste de strings.
+    du fichier, le contenu du fichier est retourné sous forme d'une liste de strings si jamais on a besoin de s'en servir.
 
     Args:
         conll_file (str): le fichier CoNLL à convertir
@@ -79,22 +59,27 @@ def convertConll(conll_file:str, converter:dict)->List[str]:
     Returns:
         file_as_list (List[str]): le contenu du nouveau fichier CoNLL converti sous forme de liste de strings
     """
+    
     file_as_list = [] # le fichier sous forme de liste si jamais on veut tester des trucs
+    
     with open(f"clustered_{conll_file.split('/')[-1]}", "w", encoding="utf-8") as output: # pour l'écriture du résultat
+    
         with open(conll_file, "r") as input_conll: # lecture du conll
             l = input_conll.readline()
+    
             while l: # tant qu'il reste des lignes
                 source = safe_list_get(l.split("\t"), 1, False) # forme du token
                 l_list = l.split("\t")
+    
                 if source in converter.keys(): # si la forme se trouve dnas le dico de conversion
                     l_list[1] = converter[source] # on remplace la forme du token par le nom du cluster
                     l_list[-1] = f"clustered = {source}\n" # on conserve la forme dans la colonne "misc"
-                elif source: # si on ne trouve pas le token dans le dico de conversion 
-                    l_list[1] = clean_token(l_list[1]) # on nettoie le token
+    
                 l = "\t".join(l_list) # on reconstitue la ligne
                 output.write(l)
                 file_as_list.append(l) 
                 l = input_conll.readline() # on passe à la ligne suivante 
+    
     return file_as_list
 
 def main():
