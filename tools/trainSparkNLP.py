ffb7e2f20ef873c2463cb94d6a9089ec90ce4997
Delphine Nguyen <delphine.nguyen-durandet@sorbonne-nouvelle.fr>
2023-01-11 15:15:42 +0100

    on peut maintenant entraîner un modèle et sauvegarder le modèle entraîné dans le dossier qu'on veut

--- tools/trainSparkNLP.py
+++ tools/trainSparkNLP.py
@@ -3,14 +3,11 @@
 import sparknlp
 from sparknlp.training import CoNLLU
 
-import pyspark.sql.functions as f
+# import pyspark.sql.functions as f
+from sparknlp.annotator import PerceptronApproach
 
-
-from pyspark.ml.feature import Word2Vec
-
+import sys
 #######################################################
-
-sc = sparknlp.start() #session de connexion
 
 # df = CoNLLU().readDataset(sc, "corpus/test/converted_aave_test.conllu") #création d'un dataframe
 
@@ -24,21 +21,42 @@ sc = sparknlp.start() #session de connexion
 
 # ################# PIPELINE #########################
 
-df_train = CoNLLU().readDataset(sc, "./Documents_Structures/clustered_clean_concatenated.conllu") #création d'un dataframe
-df_dev = CoNLLU().readDataset(sc, "./Documents_Structures/corpus/dev/aave_dev.conllu") #création d'un dataframe
-
-
-from sparknlp.annotator import PerceptronApproach
-
-approach = PerceptronApproach() \
+def main():
+    """Pour lancer le script : 
+    
+    ```bash
+    python3 trainSparkNLP.py train.conllu dev.conllu path/folder/to/save/model
+    ```
+    
+    **Note : il faut que les corpus soient déjà nettoyés**
+    """
+    
+    sc = sparknlp.start() #session de connexion
+    
+    conll_train = sys.argv[1] # corpus de train
+    conll_dev = sys.argv[2] # corpus de dev pour tester le modèle
+    model_dir = sys.argv[3] # le dossier dans lequel on veut sauvegarder le modèle
+    
+    # lecture des corpus : on store dans un dataframe
+    df_train = CoNLLU().readDataset(sc, conll_train) # corpus de train
+    df_dev = CoNLLU().readDataset(sc, conll_dev) # corpus de dev
+    
+    # pos tagger basé sur un perceptron
+    approach = PerceptronApproach() \
     .setInputCols(["document", "form"]) \
     .setOutputCol("pos") \
     .setPosColumn("upos")
 
-trained_pos_model = approach.fit(df_train) # entraînement
-annotated_df = trained_pos_model.transform(df_dev)
+    trained_pos_model = approach.fit(df_train) # entraînement
+    annotated_df = trained_pos_model.transform(df_dev) # pos tagging sur le dev avec notre modèle entraîné
 
-annotated_df.selectExpr("explode(pos.result)").show(truncate=False) # on affiche les pos prédites
+    annotated_df.selectExpr("explode(pos.result)").show(truncate=False) # on affiche les pos prédites avec une pos par ligne (explode)
 
-tags = annotated_df.selectExpr("explode(pos.result)").collect() # les pos prédites pour notre corpus de dev
+    tags = annotated_df.selectExpr("explode(pos.result)").collect() # les pos prédites pour notre corpus de dev
 
+    # sauvegarde du modèle entraîné
+    trained_pos_model.save(model_dir)
+
+
+if __name__=="__main__":
+    main()
